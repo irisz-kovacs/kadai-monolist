d0226f6b40d9edfa57eef2957b6ec452fc1798da
EC2 Default User <ec2-user@ip-172-31-8-183.us-east-2.compute.internal>
2018-06-22 04:47:08 +0000

    user registration

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -12,7 +12,7 @@
             </div>
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav navbar-right">
-                    <li><a href="#">Register</a></li>
+                     <li><a href="{{ route('signup.get') }}">Register</a></li>
                     <li><a href="#">Log in</a></li>
                 </ul>
             </div>
